9a70d70df067c295026e914c012d0d44d5121854
Vahid Viand <vahid.bidarviand@gmail.com>
2020-12-16 14:02:24 +0330

    counter created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Counter from './Counter';
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducer from "./reducer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  store = createStore(reducer)
+
+  render () {
+    return (
+      <Provider store={this.store}>
+        <Counter />
+      </Provider>
+    );
+  }
 }
 
 export default App;
---
+++ src/Counter.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Counter extends Component {
+
+    increment = () => {
+        this.props.dispatch({
+            type : "INCREMENT"
+        })
+    }
+
+    decrement = () => {
+        this.props.dispatch({
+            type : "DECREMENT"
+        })
+    }
+
+
+    render () {
+        return (
+        <div>
+            <h1>Counter</h1>
+            <div>
+                <button onClick={this.increment}>+</button>
+                <span>{this.props.count}</span>
+                <button onClick={this.decrement}>-</button>
+            </div>
+        </div>
+         );
+    }
+}
+
+const mapStateToProps = (state) =>{
+    return{
+        count : state.count
+    }
+}
+
+export default connect(mapStateToProps)(Counter);
